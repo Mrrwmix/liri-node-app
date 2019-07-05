e71e0f8526853aab7055ff6186aba8cf075bafde
Matthew Winemiller <seidoc@gmail.com>
2019-07-05 12:19:17 -0700

    Added 'The Sign' by Ace of Base if no song is searched for. Also included moment.'

--- liri.js
+++ liri.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
+const moment = require("moment");
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 const axios = require("axios");
 
 var command = process.argv[2];
-
-console.log(spotify);
 
 switch (command) {
     case 'concert-this':
@@ -28,10 +27,18 @@ function concert(band) {
 
 function song(title) {
     console.log(title);
+    if (title == '' || title == null){
+        console.log('Ace of Base - The Sign');
+        return console.log('https://open.spotify.com/track/0hrBpAOgrt8RXigk83LLNE');
+    }
     spotify
         .search({ type: 'track', query: title })
         .then(function (response) {
-            console.log(JSON.stringify(response, null, 2));
+            for (var i = 0; i < response.tracks.items.length; i++){
+                console.log(response.tracks.items[i].artists[0].name + ' - ' + response.tracks.items[i].name + ' from the album ' + response.tracks.items[i].album.name + '.');
+                console.log(response.tracks.items[i].external_urls.spotify);
+                console.log('-------------------------------------------------------------------------------------');
+            }
         })
         .catch(function (err) {
             console.log(err);
