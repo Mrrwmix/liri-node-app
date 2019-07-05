989ad2666e7c4bac1ad6c162774052b680341458
Matthew Winemiller <seidoc@gmail.com>
2019-07-05 13:22:57 -0700

    OMDB API working

--- liri.js
+++ liri.js
@@ -52,9 +52,9 @@ function song(title) {
         .search({ type: 'track', query: title })
         .then(function (response) {
             for (var i = 0; i < response.tracks.items.length; i++){
-                console.log(response.tracks.items[i].artists[0].name + ' - ' + response.tracks.items[i].name + ' from the album ' + response.tracks.items[i].album.name + '.');
-                console.log(response.tracks.items[i].external_urls.spotify);
                 console.log('-------------------------------------------------------------------------------------');
+                console.log(response.tracks.items[i].artists[0].name + ' - ' + '\"' + response.tracks.items[i].name + '\"' + ' from the album ' +  '\"' + response.tracks.items[i].album.name + '\"' +'.');
+                console.log(response.tracks.items[i].external_urls.spotify);
             }
         })
         .catch(function (err) {
@@ -63,7 +63,30 @@ function song(title) {
 }
 
 function movie(name) {
-    console.log(name);
+    var url;
+    if (name == '' || name == null || name == undefined){
+        url = "http://www.omdbapi.com/?t=mr.%2Bnobody&y=&plot=short&apikey=trilogy";
+    }
+    else {
+        url = "http://www.omdbapi.com/?t=" + name.split(' ').join('%2B') + "&y=&plot=short&apikey=trilogy"
+    }
+    axios.get(url).then(
+        function(response){
+            console.log('-------------------------------------------------------------------------------------');
+            console.log('Title: ' + response.data.Title);
+            console.log('Release Year: ' + response.data.Year);
+            console.log('IMDB Rating: ' + response.data.imdbRating);
+            if (Boolean(response.data.Ratings[1])){console.log('Rotten Tomatoes Rating: ' + response.data.Ratings[1].Value);}
+            console.log('Produced in ' + response.data.Country);
+            console.log('Language(s): ' + response.data.Language);
+            console.log('Plot: ' + response.data.Plot);
+            console.log('Actors: ' + response.data.Actors);
+            console.log('-------------------------------------------------------------------------------------');
+        }
+    )
+    .catch(function(err){
+        console.log(err);
+    })
 }
 
 function readFile() {
