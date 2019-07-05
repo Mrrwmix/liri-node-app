fc8e984daf88df949fd73c0c3e579dd7b1238da2
Matthew Winemiller <seidoc@gmail.com>
2019-07-05 11:43:12 -0700

    Spotify query working

--- liri.js
+++ liri.js
@@ -1,14 +1,15 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
 const axios = require("axios");
 
 var command = process.argv[2];
 
-console.log(command);
+console.log(spotify);
 
-switch(command){
-    case 'concert-this': 
+switch (command) {
+    case 'concert-this':
         concert(process.argv[3]);
         break;
     case 'spotify-this-song':
@@ -21,14 +22,22 @@ switch(command){
         break;
 }
 
-function concert(band){
+function concert(band) {
     console.log(band);
 }
 
-function song(title){
+function song(title) {
     console.log(title);
+    spotify
+        .search({ type: 'track', query: title })
+        .then(function (response) {
+            console.log(JSON.stringify(response, null, 2));
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
 
-function movie(name){
+function movie(name) {
     console.log(name);
 }
